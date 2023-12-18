8457da712ac457bce356bf72ef1328c263e35d0b
sayed.hussainullah <sayedsadat344@gmail.com>
2023-12-18 12:14:25 +0430

    Project Initiated

---
+++ src/main/java/com/springbootauth/springbootauthunticationandauthurization/SpringBootAuthunticationAndAuthurizationApplication.java
@@ -0,0 +1,13 @@
+package com.springbootauth.springbootauthunticationandauthurization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAuthunticationAndAuthurizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAuthunticationAndAuthurizationApplication.class, args);
+	}
+
+}
