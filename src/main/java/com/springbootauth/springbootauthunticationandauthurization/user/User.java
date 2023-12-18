89db00a2ffcc52e27db0885e4d7fd2dbe2d2d8db
sayed.hussainullah <sayedsadat344@gmail.com>
2023-12-18 12:35:41 +0430

    User entity created

---
+++ src/main/java/com/springbootauth/springbootauthunticationandauthurization/user/User.java
@@ -0,0 +1,27 @@
+package com.springbootauth.springbootauthunticationandauthurization.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "_user")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+ }
